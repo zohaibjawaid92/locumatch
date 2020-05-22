8a7e840f2560086911080cf12370741b97463fc2
zohaib <zohaibjawaid92@yahoo.com>
2020-05-22 22:47:06 +0500

    fix bug

--- config/express.js
+++ config/express.js
@@ -47,7 +47,8 @@ module.exports = function(app) {
   }));
   
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(favicon(__dirname + '/public/favicon.ico'));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
