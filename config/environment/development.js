43bbd2002452d7f308aa0effc81dc2398e7fc268
zohaibjawaid92 <zohaibjawaid92@gmail.com>
2021-01-26 19:52:16 +0500

    resolving mongo issues

--- config/environment/development.js
+++ config/environment/development.js
@@ -8,7 +8,7 @@ module.exports = {
     // uri: 'mongodb://localhost/taazaDB'
     // uri : 'mongodb://zohaib:zobi12323@ds117681.mlab.com:17681/locumatch'
     'uri' : 'mongodb+srv://zobi12323:zobi12323@cluster0.a5ikg.mongodb.net/Locumatch?retryWrites=true&w=majority'
-  },
+  , useNewUrlParser: true},
 
   seedDB: true
 };
